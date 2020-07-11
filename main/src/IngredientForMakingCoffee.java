4c42203d5b0ca6e4ff4e89e8a3e88e01a9049eb7
Edvard <edward.arustamov0@gmail.com>
2020-07-11 17:10:19 +0400

    added program which prints how much ingredients we need for making coffee.added .gitignore

--- main/src/IngredientForMakingCoffee.java
+++ main/src/IngredientForMakingCoffee.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IngredientsForCoffee {
+public class IngredientForMakingCoffee {
     public static void main (String[]args)
     {
         int water = 200;
